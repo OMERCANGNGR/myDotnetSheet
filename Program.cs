eac6d65b6309134d1837627e244598634d23a121
ÖMER <omercangungor45@gmail.com>
2023-09-28 15:33:53 +0300

    my csharp reminder

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace myApp
+{
+    class Program{
+        static void Main(string[] args){
+         
+       /*  int a =5;
+        int b= 5;
+        int flag= a>b ? 1 : a<b ? -1: 0 ;
+        Console.WriteLine(flag); */
+
+        
+       /*  string? fname=Console.ReadLine();
+        string? lname =Console.ReadLine();
+        Console.WriteLine($"{fname} {lname}"); */
+        
+        
+       /*  var i =2;
+        var j =2;
+        var c = i/j;
+        var d = i%j;
+        var e = $"{c}.{d}";
+        Console.WriteLine(e);
+ */     
+ 
+        /* string? switch_on=Console.ReadLine();
+        switch (switch_on)
+        {
+            case "a":
+                Console.WriteLine($"{switch_on}");
+                break;
+            case "b":
+                Console.WriteLine($"{switch_on}");
+                break;
+            default:
+            Console.WriteLine("unexpected");
+            break;
+        } */
+        
+        /* var x =1;
+        var y =2;
+        var z =3;
+        var tupleN = (x,y,z);
+
+        switch (tupleN)
+        {
+            case (1,2,3):
+            //                Console.WriteLine($"case {(int)(x)} {(int)(y)} {(int)(z)}");
+                Console.WriteLine("Correct");
+                break;
+            case (1,2,2):
+                Console.WriteLine("Incorrect");
+                break;
+            default:
+                Console.WriteLine("unexpected");
+                break;
+        } */
+        
+        
+        /* int t = 1;
+        float v= 1.2f;
+        Console.WriteLine(t+v); */
+
+       /*  Console.WriteLine("Please enter your name");
+        string? inputName = Console.ReadLine();
+        Console.WriteLine("Please enter your last name");
+        string? inputLastName = Console.ReadLine();
+        Console.WriteLine($"{inputName} {inputLastName}");
+ */     
+ 
+ 
+        /* 
+        string person="testP1";
+        string person2="testP2";
+        string person3="testP3";
+        ArrayList arrayList = new ArrayList();
+        arrayList.Add(person);
+        arrayList.Add(person2);
+        arrayList.Add(person3);
+        
+        foreach (var item in arrayList)
+        {
+            Console.WriteLine(item);
+        }
+        arrayList.RemoveAt(0);
+        Console.WriteLine("-------------------");
+        Console.WriteLine(arrayList[0]);
+        Console.WriteLine("-------------------");
+        Console.WriteLine(arrayList.Count);
+
+        foreach (var item in arrayList)
+        {
+            Console.WriteLine(item);
+        }
+
+
+        object?[] people = arrayList.ToArray();
+        
+        foreach (var item in people)
+        {
+            Console.WriteLine(item);
+        }
+
+ */
+        
+        
+
+
+        }
+    }
+}
---
+++ obj/NewClass.cs
@@ -0,0 +1,9 @@
+namespace myApp.obj
+{
+    public class NewClass
+    {
+        double a = 10;
+        double b = 21;
+        console.WriteLine(b/a);
+    }
+}
